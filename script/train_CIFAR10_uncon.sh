4692bbbc61d338d411e0224b6141d7c0a3bfe297
Murasamy <peiyang7@illinois.edu>
2025-04-13 16:44:49 -0500

    Update train_CIFAR10_uncon.sh

--- script/train_CIFAR10_uncon.sh
+++ script/train_CIFAR10_uncon.sh
@@ -1,3 +1,8 @@
+export PATH="/u/pwu3/miniconda3/envs/python6/bin:/u/pwu3/miniconda3/bin:/u/pwu3/miniconda3/condabin:$PATH"
+source ~/miniconda3/etc/profile.d/conda.sh;
+conda activate python6;
+wandb login 521eacb01d9cd4e083cf5a065774992dd857a527;
+
 CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py --train  \
         --flagfile ./config/cifar100.txt --parallel \
         --logdir ./logs/cifar100lt_ddpm --total_steps 300001 \
