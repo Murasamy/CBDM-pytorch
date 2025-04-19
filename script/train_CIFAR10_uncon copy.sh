d05f756dd259944fc7215707ca1251b68e3ddca3
Murasamy <peiyang7@illinois.edu>
2025-04-18 21:17:22 -0500

    script

--- script/train_CIFAR10_uncon copy.sh
+++ script/train_CIFAR10_uncon copy.sh
@@ -1,7 +1,6 @@
 export PATH="/u/pwu3/miniconda3/envs/python6/bin:/u/pwu3/miniconda3/bin:/u/pwu3/miniconda3/condabin:$PATH"
 source ~/miniconda3/etc/profile.d/conda.sh;
 conda activate python6;
-wandb login 521eacb01d9cd4e083cf5a065774992dd857a527;
 
 python main.py --train  \
         --flagfile ./config/cifar100.txt --parallel \
