b3625a35e53d032f2693a6161ef9b3464585234b
Murasamy <peiyang7@illinois.edu>
2025-04-21 01:20:47 -0500

    Update seperate_conditional.sh

--- script/seperate_conditional.sh
+++ script/seperate_conditional.sh
@@ -1,5 +1,3 @@
-conda activate opence-v1.6.1;
-
 CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py --train  \
         --flagfile ./config/cifar100.txt --parallel \
         --logdir ./logs/cifar100lt_seperate_umsampler \
