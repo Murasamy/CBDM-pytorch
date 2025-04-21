f21f556263509f2e6cc4a932a1db694443558d3b
Murasamy <peiyang7@illinois.edu>
2025-04-21 16:26:07 -0500

    Update train_CIFAR10_uncon_truncate.sh

--- script/train_CIFAR10_uncon_truncate.sh
+++ script/train_CIFAR10_uncon_truncate.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py --train  \
         --flagfile ./config/cifar100.txt --parallel \
-        --logdir ./logs/cifar100lt_ddpm --total_steps 300001 \
+        --logdir ./logs/cifar100lt_ddpm_truncate --total_steps 300001 \
         --edm2_truncate True --edm2_truncate_portion 0.7 \
         --conditional \
         --data_type cifar100lt --imb_factor 0.01 --img_size 32 \
