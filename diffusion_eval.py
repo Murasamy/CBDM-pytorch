193b7347e629d77e104c6f396d900d5c61e7b994
Murasamy <peiyang7@illinois.edu>
2025-05-20 23:59:06 -0500

    Update diffusion_eval.py

--- diffusion_eval.py
+++ diffusion_eval.py
@@ -191,6 +191,7 @@ class GaussianDiffusionSampler(nn.Module):
         x_t = x_T.clone()
         y = None
         print('x_T', x_T.shape) # torch.Size([16, 3, 32, 32])
+        print('selected_labels', selected_labels.shape) # torch.Size([16, 3, 32, 32])
 
         if method == 'uncond':
             y = None
