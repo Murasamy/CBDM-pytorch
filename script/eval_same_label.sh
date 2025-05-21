936a8f7581f3038de55c7242c77ae65bf6ebba49
Murasamy <peiyang7@illinois.edu>
2025-05-21 08:43:22 -0500

    Update eval_same_label.sh

--- script/eval_same_label.sh
+++ script/eval_same_label.sh
@@ -4,7 +4,7 @@ python main_eval.py \
     --flagfile ./logs/cifar100lt_ddpm_cond/flagfile.txt \
     --logdir ./logs/cifar100lt_ddpm_cond \
     --fid_cache ./stats/cifar100.train.npz \
-    --ckpt_step 300000 \
+    --ckpt_step 200000 \
     --num_images 50000 --batch_size 512 \
     --notrain \
     --eval \
