de0f9d4be5563f9a053ca5c9d50175f9e74c356f
Murasamy <peiyang7@illinois.edu>
2025-05-20 23:47:41 -0500

    Update eval_same_label.sh

--- script/eval_same_label.sh
+++ script/eval_same_label.sh
@@ -1,5 +1,5 @@
 # only use cuda1
-CUDA_VISIBLE_DEVICES=0 python main_eval.py \
+CUDA_VISIBLE_DEVICES=1 python main_eval.py \
     --flagfile ./logs/cifar100lt_ddpm_cond/flagfile.txt \
     --logdir ./logs/cifar100lt_ddpm_cond \
     --fid_cache ./stats/cifar100.train.npz \
@@ -9,6 +9,5 @@ CUDA_VISIBLE_DEVICES=0 python main_eval.py \
     --eval \
     --same_label True \
     --sample_method cfg  --omega 0.8 \
-    --data_type cifar100lt8 \
-    --data_type cifar10lt
+    --data_type cifar100lt
 
