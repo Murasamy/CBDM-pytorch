9807ca443942ecb3290952537206082d088d10ec
Murasamy <peiyang7@illinois.edu>
2025-05-22 11:10:02 -0500

    Update eval_same_label.sh

--- script/eval_same_label.sh
+++ script/eval_same_label.sh
@@ -9,32 +9,20 @@ python main_eval.py \
     --notrain \
     --eval \
     --same_label True \
-    --sample_method cfg  --omega 0.8 \
+    --sample_method cfg  --omega 1 \
     --data_type cifar100lt
 
-python main_eval.py \
-    --flagfile ./logs/cifar100lt_ddpm_cond/flagfile.txt \
-    --logdir ./logs/cifar100lt_ddpm_cond \
-    --fid_cache ./stats/cifar100.train.npz \
-    --ckpt_step 50000 \
-    --num_images 10847 --batch_size 512 \
-    --notrain \
-    --eval \
-    --same_label True \
-    --sample_method cfg  --omega 1.6 \
-    --data_type cifar100lt
-
-python main_eval.py \
-    --flagfile ./logs/cifar100lt_ddpm_cond/flagfile.txt \
-    --logdir ./logs/cifar100lt_ddpm_cond \
-    --fid_cache ./stats/cifar100.train.npz \
-    --ckpt_step 100000 \
-    --num_images 10847 --batch_size 512 \
-    --notrain \
-    --eval \
-    --same_label True \
-    --sample_method cfg  --omega 0.8 \
-    --data_type cifar100lt
+# python main_eval.py \
+#     --flagfile ./logs/cifar100lt_ddpm_cond/flagfile.txt \
+#     --logdir ./logs/cifar100lt_ddpm_cond \
+#     --fid_cache ./stats/cifar100.train.npz \
+#     --ckpt_step 50000 \
+#     --num_images 10847 --batch_size 512 \
+#     --notrain \
+#     --eval \
+#     --same_label True \
+#     --sample_method cfg  --omega 1.6 \
+#     --data_type cifar100lt
 
 python main_eval.py \
     --flagfile ./logs/cifar100lt_ddpm_cond/flagfile.txt \
@@ -45,8 +33,20 @@ python main_eval.py \
     --notrain \
     --eval \
     --same_label True \
-    --sample_method cfg  --omega 1.6 \
+    --sample_method cfg  --omega 1 \
     --data_type cifar100lt
+
+# python main_eval.py \
+#     --flagfile ./logs/cifar100lt_ddpm_cond/flagfile.txt \
+#     --logdir ./logs/cifar100lt_ddpm_cond \
+#     --fid_cache ./stats/cifar100.train.npz \
+#     --ckpt_step 100000 \
+#     --num_images 10847 --batch_size 512 \
+#     --notrain \
+#     --eval \
+#     --same_label True \
+#     --sample_method cfg  --omega 1.6 \
+#     --data_type cifar100lt
 
 python main_eval.py \
     --flagfile ./logs/cifar100lt_ddpm_cond/flagfile.txt \
@@ -57,17 +57,17 @@ python main_eval.py \
     --notrain \
     --eval \
     --same_label True \
-    --sample_method cfg  --omega 0.8 \
+    --sample_method cfg  --omega 1 \
     --data_type cifar100lt
 
-python main_eval.py \
-    --flagfile ./logs/cifar100lt_ddpm_cond/flagfile.txt \
-    --logdir ./logs/cifar100lt_ddpm_cond \
-    --fid_cache ./stats/cifar100.train.npz \
-    --ckpt_step 150000 \
-    --num_images 10847 --batch_size 512 \
-    --notrain \
-    --eval \
-    --same_label True \
-    --sample_method cfg  --omega 1.6 \
-    --data_type cifar100lt
+# python main_eval.py \
+#     --flagfile ./logs/cifar100lt_ddpm_cond/flagfile.txt \
+#     --logdir ./logs/cifar100lt_ddpm_cond \
+#     --fid_cache ./stats/cifar100.train.npz \
+#     --ckpt_step 150000 \
+#     --num_images 10847 --batch_size 512 \
+#     --notrain \
+#     --eval \
+#     --same_label True \
+#     --sample_method cfg  --omega 1.6 \
+#     --data_type cifar100lt
