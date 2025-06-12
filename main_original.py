8290e490d9af287629bcf17fef02b1d99dde36fc
Murasamy <peiyang7@illinois.edu>
2025-06-12 00:01:11 -0500

    Update main_original.py

--- main_original.py
+++ main_original.py
@@ -15,10 +15,10 @@ from torchvision import transforms
 
 from diffusion_original import GaussianDiffusionTrainer, GaussianDiffusionSampler
 from model.model_original import UNet
-from utils.augmentation import *
+# from utils.augmentation import *
 from dataset import ImbalanceCIFAR100, ImbalanceCIFAR10
 from score.both import get_inception_and_fid_score
-from utils.augmentation import KarrasAugmentationPipeline
+# from utils.augmentation import KarrasAugmentationPipeline
 
 
 FLAGS = flags.FLAGS
